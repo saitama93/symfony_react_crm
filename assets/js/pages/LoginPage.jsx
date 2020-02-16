60f54a5dea99b42aabef347fb9971ce7d9b31edd
saimata93 <igalilmi32@gmail.com>
2020-02-16 12:53:30 +0000

    Création du composant <Field/>

--- assets/js/pages/LoginPage.jsx
+++ assets/js/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import AuthAPI from "../services/authAPI";
 import AuthContext from "../contexts/AuhtContext";
+import Field from "../components/forms/Field";
 
 const LoginPage = ({ history }) => {
   const { setIsAuthenticated } = useContext(AuthContext);
@@ -40,31 +41,21 @@ const LoginPage = ({ history }) => {
       <h1>Connexion</h1>
 
       <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Email</label>
-          <input
-            value={credentials.username}
-            onChange={handleChange}
-            type="email"
-            className={"form-control" + (error && " is-invalid")}
-            placeholder="Adresse email de connexion"
-            name="username"
-            id="username"
-          />
-          {error && <p className="invalid-feedback">{error}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Mot de passe</label>
-          <input
-            value={credentials.password}
-            onChange={handleChange}
-            type="password"
-            className="form-control"
-            placeholder="Mot de passe"
-            name="password"
-            id="password"
-          />
-        </div>
+        <Field
+          label="Email"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+          placeholder="Adresse email de connexion"
+          error={error}
+        />
+        <Field
+          label="Mot de passe"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+          type="password"
+        />
 
         <div className="form-group">
           <button type="submit" className="btn btn-success">
